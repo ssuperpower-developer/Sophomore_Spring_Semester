33697519749fc9e8bd9f16f04a174cebecc2dad9
rover0811 <rover0811@hotmail.com>
2022-04-25 15:52:23 +0900

    Add commit for OOP_second work

---
+++ 2-1_ObjectOrientedProgramming/KHS/객체지향 2번째 과제/Num06.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Num06 {
+    public static void main(String[] args) {
+        int [] init={50000,10000,1000,500,100,50,10,1};
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("금액을 입력하세요 >>");
+        int inputNum= scanner.nextInt();
+        int temp=inputNum;
+        for (int i=0;i<init.length;i++){
+            System.out.println(init[i]+"원 짜리 :"+inputNum/init[i]);
+            inputNum-=(inputNum/init[i])*init[i];
+
+        }
+    }
+}
---
+++ 2-1_ObjectOrientedProgramming/KHS/객체지향 2번째 과제/Num12.java
@@ -0,0 +1,5 @@
+public class Num12 {
+    public static void main(String[] args) {
+
+    }
+}
