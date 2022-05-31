d971d14126cd99f6f4f738dba40dec00dddfd239
rover0811 <rover0811@hotmail.com>
2022-05-31 16:13:08 +0900

    Finish OOP_5th Workout

--- 2-1_ObjectOrientedProgramming/KHS/객체지향 5번째 과제/PairMap.java
+++ 2-1_ObjectOrientedProgramming/KHS/객체지향 5번째 과제/PairMap.java
@@ -1,93 +1,85 @@
 abstract class PairMap {
     protected String keyArray[];
     protected String valueArray[];
-
     abstract String get(String key);
-
     abstract void put(String key, String value);
-
     abstract String delete(String key);
-
     abstract int length();
 }
 
 class Dictionary extends PairMap {
     private int indexChecker = 0;
-    private int itemChecker = 0;
-
-    Dictionary() {
-    }
+    private int size;
 
     Dictionary(int size) {
         keyArray = new String[size];
         valueArray = new String[size];
+        this.size = size;
     }
 
     @Override
-    String get(String key)//key값을 지닌
+    String get(String key)
     {
         int i = 0;
+        boolean isNull = true;
         for (; i < keyArray.length; i++) {
-            if (keyArray[i]==key) {
-                return valueArray[i];
+            if (key.equals(keyArray[i])) {
+                isNull = false;
+                break;
             }
         }
-        return null;//check Num
-    }
-
-    int getIndex(String key)//for index
-    {
-        int i = 0;
-        for (; i < keyArray.length; i++) {
-            if (keyArray[i]==key) break;
+        if (isNull == false) {
+            return keyArray[i];
+        } else {
+            return null;
         }
-        if (i == keyArray.length) return -1;//check Num for Exception
-        return i;
     }
 
     @Override
     void put(String key, String value) {
-        int temp = getIndex(key);
-
-
-        try {
-            if (indexChecker == 0) {
-                keyArray[0] = key;
-                valueArray[0] = value;
-                indexChecker++;
-                itemChecker++;
-            } else if (indexChecker != 0 && (get(key) != null)) {
-                valueArray[temp] = value;
-            } else if(indexChecker != 0 && (get(key) == null)){
-                keyArray[indexChecker] = key;
-                valueArray[indexChecker] = value;
-                indexChecker++;
-                itemChecker++;
+        if (get(key) == null) {
+            keyArray[indexChecker] = key;
+            valueArray[indexChecker] = value;
+            indexChecker++;
+        } else {
+            int i = 0;
+            boolean isNull = true;
+            for (; i < keyArray.length; i++) {
+                if (key.equals(keyArray[i])) break;
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("");
+            valueArray[i] = value;
         }
     }
 
     @Override
     String delete(String key) {
-        int temp = getIndex(key);
-        if (temp != -1) {
-            String temps = valueArray[temp];
-            keyArray[temp] = null;
-            valueArray[temp] = null;
-            return temps;
+        if(!get(key).equals(key)){
+            System.out.println("해당하는 값은 없습니다");
+            return "";
         }
-        return null;
+        else {
+            int i=-1;
+            for (int j = 0; j <keyArray.length ; j++) {
+                if (key.equals(keyArray[j])) {
+                    i=j;
+                    keyArray[j]=null;
+                    valueArray[j]=null;
+                    break;
+                }
+            }
+            return valueArray[i];
+        }
     }
 
     @Override
     int length() {
-        int temp = 0;
-        for (int i = 0; i < keyArray.length; i++) {
-            if (keyArray[i] != null) temp++;
+        int counter = 0;
+        for (int i=0; i < size; i++) {
+            if (keyArray[i] != null) {
+                counter++;
+            }
         }
-        return temp;
+        return counter;
     }
 }
 
@@ -100,6 +92,6 @@ class DictionaryApp {
         System.out.println("이재문의 값은 " + dic.get("이재문"));
         System.out.println("황기태의 값은 " + dic.get("황기태"));
         dic.delete("황기태");
-        System.out.println("황기태의 값은" + dic.get("황기태"));
+        System.out.println("황기태의 값은 " + dic.get("황기태"));
     }
 }
